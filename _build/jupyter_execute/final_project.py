ed8c715b7a5f1d3d253622a6694cb78cce227536
Filipa Calado <gofilipa@gmail.com>
2023-05-11 17:42:41 -0400

    updating final assignment

--- _build/jupyter_execute/final_project.py
+++ _build/jupyter_execute/final_project.py
@@ -16,7 +16,7 @@
 # 
 # Steps: 
 # - First, you will load up a dataset for analysis. You may choose from the following sets of data:
-#    - In the default option, this dataset is "[toxcicity
+#    - In the default option, this dataset is "[toxicity
 #   prompts](https://huggingface.co/datasets/allenai/real-toxicity-prompts),"
 #   which is a collection of prompts that tend to provoke biased
 #   responses. You will load this dataset using the provided code in the
@@ -29,7 +29,7 @@
 #   code provided.
 # - Third, you will create a generator object tied into which you will
 #   pass your prompts. Here, you may use the default language model
-#   (which is 'distil-gpt2', or use one of your choosing from the
+#   (which is 'gpt2', or use one of your choosing from the
 #   [models
 #   hub](https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads)),
 #   with the "text generation" option from the "Natural Language
@@ -42,6 +42,6 @@
 #     - Pay attention to anything in the results that sticks out to you. Are there any surprising results? Or any patterns that you can notice across multiple results? What do these surprises or patterns in the results suggest about the training data?   
 #     - Do you find the results to be more biased or offensive than the original prompt? Do
 #     you find them to be more biased or offensive than the "continuation" sentences from the original dataset? How so?
-#     - What do the results say about the original dataset? What ideas or biases might be prevalent in the original dataset?
+#     - What do the results say about the original training data? What ideas or biases might be prevalent in the original dataset?
 
 # 
